66df9dc42cc8e1b7535a3d72f390bebbec8703e3
Yunjeong <yunjeongiya@gmail.com>
2025-05-26 09:48:39 +0900

    fix: routing problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -21,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/checkus-student-mobile">
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/checkus-student-mobile/',
   server: {
     host: "::",
-    port: 8080,
+    port: 3002,
   },
   plugins: [
     react(),
